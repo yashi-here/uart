3eff904c68232800fa7f517b5ffb39ad7e10b1f9
gsv9 <gsaivishesh@gmail.com>
2026-03-11 09:21:54 +0530

    Update baud_counter.v

--- baud_counter.v
+++ baud_counter.v
@@ -7,28 +7,33 @@ module baud_counter(
 );
 
 reg [15:0] counter;
-reg [1:0] edge_count;
+reg [2:0] edge_count;
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        counter <= 0;
-        baud_count <= 0;
-        baud_valid <= 0;
-        edge_count <= 0;
+        counter     <= 0;
+        baud_count  <= 0;
+        baud_valid  <= 0;
+        edge_count  <= 0;
     end
     else begin
 
-        counter <= counter + 1;
+        // Count clock cycles only until baud detected
+        if(!baud_valid)
+            counter <= counter + 1;
 
-        if(edge_pulse) begin
+        if(edge_pulse && !baud_valid) begin
 
             edge_count <= edge_count + 1;
 
-            if(edge_count == 0)
+            // First edge → start measurement
+            if(edge_count == 0) begin
                 counter <= 0;
+            end
 
+            // Second edge → capture bit period
             else if(edge_count == 1) begin
-                baud_count <= counter; 
+                baud_count <= counter;
                 baud_valid <= 1;
             end
         end
