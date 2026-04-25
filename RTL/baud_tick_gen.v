1fa31f994815f2e8707e26cd84585c152e8e4f48
gsv9 <gsaivishesh@gmail.com>
2026-04-25 11:53:31 +0530

    Update baud_tick_gen.v

--- RTL/baud_tick_gen.v
+++ RTL/baud_tick_gen.v
@@ -6,35 +6,36 @@ module baud_tick_gen(
     output reg sample_tick,
     output reg bit_tick
 );
+
 reg [15:0] counter;
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        counter <= 0;
-        sample_tick <= 0;
-        bit_tick <= 0;
+        counter <= 16'd0;
+        sample_tick <= 1'b0;
+        bit_tick <= 1'b0;
     end
+    else if (baud_valid) begin
+        sample_tick <= 1'b0;
+        bit_tick <= 1'b0;
 
-    else if(baud_valid) begin
-
-        sample_tick <= 0;
-        bit_tick <= 0;
-
-        if(counter >= baud_count) begin
-            counter <= 0;
-            bit_tick <= 1;
+        if (counter >= baud_count) begin
+            counter <= 16'd0;
+            bit_tick <= 1'b1;
         end
         else begin
-            counter <= counter + 1;
+            counter <= counter + 16'd1;
         end
 
-        if(counter == (baud_count >> 1))
-            sample_tick <= 1;
-
+        if (counter == (baud_count >> 1)) begin
+            sample_tick <= 1'b1;
+        end
     end
-
     else begin
-        counter <= 0;
+        counter <= 16'd0;
+        sample_tick <= 1'b0;
+        bit_tick <= 1'b0;
     end
-    end
+end
+
 endmodule
